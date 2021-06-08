eb1ad8c03b6dbcea3cc0f2279022ce2c55c34900
Gabriel Nicol <moraesnicol@gmail.com>
2021-06-08 19:43:23 -0300

    navigation bar OK

--- tabs_ios/ViewController.swift
+++ tabs_ios/ViewController.swift
@@ -12,8 +12,42 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        title = "Termos e condições"
+    
+        
+        configureTopBar()
+        
     }
-
-
+    
+    
+    //
+    private func configureTopBar() {
+        let buttonRightIcon = UIImage(named: "closebutton")
+        let buttonLeftIcon = UIImage(named: "backbutton")
+        
+            
+        let navigationItem = navigationItem
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(image: buttonRightIcon, style: UIBarButtonItem.Style.plain, target: self, action: nil)
+        
+        navigationItem.leftBarButtonItem = UIBarButtonItem(image: buttonLeftIcon, style: UIBarButtonItem.Style.plain, target: self, action: nil)
+        
+        
+        let navigationBar = navigationController?.navigationBar
+       
+        navigationBar?.barTintColor = UIColor.white
+        navigationBar?.isTranslucent = true
+        navigationBar?.shadowImage = UIImage()
+        navigationBar?.titleTextAttributes = [.foregroundColor: UIColor.black.withAlphaComponent(0.5)]
+        
+     
+        
+  
+        
+       //
+        
+        
+    }
+    
 }
 
